a651ffad26b5532495dbe94f65ce977789c280da
jsjzh <kimimi_king@163.com>
2019-05-28 15:14:51 +0800

    edit

--- src/index.js
+++ src/index.js
@@ -4,11 +4,9 @@ const { Range, Position } = vscode
 const Color = require('./Color')
 const { testColor } = require('./utils')
 
-/**
- * @param {vscode.ExtensionContext} context
- */
-function activate(context) {
-  console.log('color converter is activate~')
+exports.activate = context => {
+  console.log('color converter is activate')
+
   function colorConverter() {
     const { activeTextEditor } = vscode.window
     const { selections, document } = activeTextEditor
@@ -23,26 +21,21 @@ function activate(context) {
     const preRanges = prePositions.map(validPositions => new Range(validPositions[0], validPositions[1]))
     // 获取有效的 Ranges，该数组中的为用户选中并且通过了色值 reg 的过滤的 ranges
     const validRanges = preRanges.filter(range => testColor.some(regObj => regObj.reg.test(document.getText(range))))
-
-    const currRanges = validRanges.map(range => new Color(document.getText(range)))
-
+    // 存储每个色值的类
+    const colors = validRanges.map(range => new Color(document.getText(range)))
+    // 实际实现修改的方法
     activeTextEditor.edit(editBuilder => {
       validRanges.forEach((range, index) => {
-        editBuilder.replace(range, currRanges[index].getColor())
+        editBuilder.replace(range, colors[index].getColor())
       })
     })
-
-    vscode.window.showInformationMessage('color converter say hello~')
   }
 
-  context.subscriptions.push(vscode.commands.registerCommand('extension.cc', colorConverter))
+  const realCommand = vscode.commands.registerCommand('extension.cc', colorConverter)
+
+  context.subscriptions.push(realCommand)
 }
 
-function deactivate() {
-  console.log('color converter is deactivate~')
-}
-
-module.exports = {
-  activate,
-  deactivate
+exports.deactivate = () => {
+  console.log('color converter is deactivate')
 }
